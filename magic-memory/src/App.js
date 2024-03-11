b94b6a0eb7b27ed479bbca51faab6fe90c241c39
MissC1010 <celina.indhur@hotmail.com>
2024-03-11 15:43:47 +0200

    commented code

--- magic-memory/src/App.js
+++ magic-memory/src/App.js
@@ -10,7 +10,8 @@ const cardImages = [
   { src: "/img/shield-1.png", matched: false },
   { src: "/img/sword-1.png", matched: false },
 ];
-
+//usestate provides two elements, card- state var and setcards- function to update state var
+//The useState([]) initializes the state variable (cards) with an empty array.
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
@@ -31,16 +32,28 @@ function App() {
   };
 
   //handle a choice
+  // If choiceOne is truthy (not null), it means the user has already selected one card.
+  // We update choiceTwo with the current card as the second choice.
+  // If choiceOne is falsy (null), it means the user is making the first card selection.
+  // We update choiceOne with the current card as the first choice.
   const handleChoice = (card) => {
+    // The condition below uses the ternary operator (?:)
+    // It's a shorthand way of writing an if-else statement.
+    // If the condition before the '?' is true, it executes the expression after the '?',
+    // otherwise, it executes the expression after the ':'.
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
 
   //compare 2 selected cards
   useEffect(() => {
+    // Check if both choices have been made by the user
     if (choiceOne && choiceTwo) {
+      // Disable further card clicks during comparison
       setDisabled(true);
 
+      // Check if the selected cards have the same image source
       if (choiceOne.src === choiceTwo.src) {
+        // If cards match, mark them as matched and reset the turn
         setCards((prevCards) => {
           return prevCards.map((card) => {
             if (card.src === choiceOne.src) {
